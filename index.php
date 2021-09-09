d127daa920c71e15ff570d15f310689fbf3699e0
adrian <adrian@onlinemeded.org>
2021-09-09 16:39:00 -0500

    removed demo code

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <?php
 
-use App\Interfaces\Deck;
-use App\Standard\StandardTheme;
-
 require __DIR__ . '/vendor/autoload.php';
 
 \Psy\Shell::debug(get_defined_vars());
-
-$p = new \App\Standard\Suits\Colors\Red();
-$s = new \App\Standard\Suits\Colors\Black();
-
-$theme = new StandardTheme($p, $s);
-$attr = new \App\Standard\StandardStandardCardAttributes($theme);
-$gen = new \App\Generators\StandardDeckGenerator($attr);
-$deck = $gen->generate();
-
-class Controller
-{
-    public function __invoke(Deck $deck)
-    {
-        // TODO: Implement __invoke() method.
-    }
-}
-
